198c6c5d4b0301b373a0ca3019eb826fd3851e82
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-02-22 12:37:13 +0200

    ADD: add logic for condition rendering buttons in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { SIGN_IN_PATH, SIGN_UP_PATH } from "../../constants/paths";
+
 const Header = () => {
-    return <header>Header</header>;
+    let location = useLocation();
+    const [isLogin, setIsLogin] = useState(false);
+    useEffect(() => {
+        if (
+            location.pathname === SIGN_IN_PATH ||
+            location.pathname === SIGN_UP_PATH
+        )
+            setIsLogin(true);
+    }, []);
+
+    return (
+        <header>
+            Header
+            {isLogin ? <button>one</button> : null}
+        </header>
+    );
 };
 
 export default Header;
