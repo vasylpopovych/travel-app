bcd436a394cca901b13528ef23576b584e629334
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-02-23 19:47:44 +0200

    ADD: add styles for sign-up page

--- src/pages/signUpPage/SignUpPage.js
+++ src/pages/signUpPage/SignUpPage.js
@@ -5,12 +5,12 @@ import Button from "../../components/UI/button/Button";
 import Input from "../../components/UI/input/Input";
 import { Link, useNavigate } from "react-router-dom";
 import { HOME_PAGE_PATH, SIGN_IN_PATH } from "../../constants/paths";
+import styles from "./signUpPage.module.css";
 
 const SignUpPage = () => {
     const [fullName, setFullName] = useState(null);
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
-    const [isDataValid, setIsDataValid] = useState(null);
     const navigate = useNavigate();
 
     const handleFullNameData = (data) => {
@@ -27,65 +27,62 @@ const SignUpPage = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (getValidationForm(fullName, email, password)) {
-            setIsDataValid(true);
+        if (getValidationForm(fullName, email, password))
             navigate(HOME_PAGE_PATH);
-        } else {
-            setIsDataValid(false);
-        }
     };
 
     return (
         <Layout>
-            <h1 className="visually-hidden">Travel App</h1>
-            <form
-                onSubmit={handleSubmit}
-                className="sign-up-form"
-                autoComplete="off"
-            >
-                <h2 className="sign-up-form__title">Sign Up</h2>
-                <Input
-                    onInputData={handleFullNameData}
-                    labelName={"Full name"}
-                    data-test-id="auth-full-name"
-                    name="full-name"
-                    type="text"
-                    required
-                />
-                <Input
-                    onInputData={handleEmailData}
-                    labelName={"Email"}
-                    data-test-id="auth-email"
-                    name="email"
-                    type="email"
-                    required
-                />
-                <Input
-                    onInputData={handlePasswordData}
-                    labelName={"Password"}
-                    data-test-id="auth-password"
-                    name="password"
-                    type="password"
-                    autoComplete="new-password"
-                    required
-                />
-                <Button
-                    data-test-id="auth-submit"
-                    className="button"
-                    type="submit"
-                />
-            </form>
-
-            <span>
-                Already have an account?
-                <Link
-                    data-test-id="auth-sign-in-link"
-                    to={SIGN_IN_PATH}
-                    className="sign-up-form__link"
+            <main className={styles.sign_up_page}>
+                <h1 className="visually-hidden">Travel App</h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className={styles.sign_up_form}
+                    autoComplete="off"
                 >
-                    Sign In
-                </Link>
-            </span>
+                    <h2 className={styles.sign_up_form__title}>Sign Up</h2>
+                    <Input
+                        onInputData={handleFullNameData}
+                        labelName={"Full name"}
+                        data-test-id="auth-full-name"
+                        name="full-name"
+                        type="text"
+                        required
+                    />
+                    <Input
+                        onInputData={handleEmailData}
+                        labelName={"Email"}
+                        data-test-id="auth-email"
+                        name="email"
+                        type="email"
+                        required
+                    />
+                    <Input
+                        onInputData={handlePasswordData}
+                        labelName={"Password"}
+                        data-test-id="auth-password"
+                        name="password"
+                        type="password"
+                        autoComplete="new-password"
+                        required
+                    />
+                    <Button
+                        data-test-id="auth-submit"
+                        className="button"
+                        type="submit"
+                    />
+                </form>
+                <span>
+                    Already have an account?
+                    <Link
+                        data-test-id="auth-sign-in-link"
+                        to={SIGN_IN_PATH}
+                        className={styles.sign_up_form__link}
+                    >
+                        Sign In
+                    </Link>
+                </span>
+            </main>
         </Layout>
     );
 };
