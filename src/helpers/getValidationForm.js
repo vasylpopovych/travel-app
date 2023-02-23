e280d92d7126d7dc9b979477e5a2c4b70da5902e
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-02-23 20:38:37 +0200

    CHANGE: change order of arguments in function

--- src/helpers/getValidationForm.js
+++ src/helpers/getValidationForm.js
@@ -2,7 +2,9 @@ const fullNameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegex = /^.{3,20}$/;
 
-export const getValidationForm = (fullName = "not-exist", email, pass) => {
+export const getValidationForm = (email, pass, fullName = "not-exist") => {
+    console.log("validating...");
+    console.log(`fullname: ${fullName}`);
     if (fullName === "not-exist") {
         if (emailRegex.test(email.trim()) && passwordRegex.test(pass.trim()))
             return true;
