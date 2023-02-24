fb296b37bcbcdd0fae98e520a2036dc175b382d8
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-02-24 07:54:23 +0200

    ADD: add all styles and some functionality

---
+++ src/components/tripsFilter/TripsFilter.js
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+import styles from "./tripsFilter.module.css";
+
+const TripsFilter = ({ onSearch, onSelectLevel, onSelectDuration }) => {
+    const handleInputData = (event) => {
+        onSearch(event.target.value.toLowerCase().trim());
+    };
+
+    const handleSelectLevel = (event) => {
+        onSelectLevel(event.target.value);
+    };
+
+    const handleSelectDuration = (event) => {
+        onSelectDuration(event.target.value);
+    };
+    return (
+        <section className={styles.trips_filter}>
+            <h2 className="visually-hidden">Trips filter</h2>
+            <form className={styles.trips_filter__form} autoComplete="off">
+                <label className={`${styles.trips_filter__search} input`}>
+                    <span className="visually-hidden">Search by name</span>
+                    <input
+                        onChange={handleInputData}
+                        data-test-id="filter-search"
+                        name="search"
+                        type="search"
+                        placeholder="search by title"
+                    />
+                </label>
+                <label className={styles.select}>
+                    <span className="visually-hidden">Search by duration</span>
+                    <select
+                        data-test-id="filter-duration"
+                        name="duration"
+                        onChange={handleSelectDuration}
+                    >
+                        <option value="">duration</option>
+                        <option value="0_x_5">&lt; 5 days</option>
+                        <option value="5_x_10">&lt; 10 days</option>
+                        <option value="10_x">&ge; 10 days</option>
+                    </select>
+                </label>
+                <label className={styles.select}>
+                    <span className="visually-hidden">Search by level</span>
+                    <select
+                        data-test-id="filter-level"
+                        name="level"
+                        onChange={handleSelectLevel}
+                    >
+                        <option value="">level</option>
+                        <option value="easy">easy</option>
+                        <option value="moderate">moderate</option>
+                        <option value="difficult">difficult</option>
+                    </select>
+                </label>
+            </form>
+        </section>
+    );
+};
+
+export default TripsFilter;
--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,16 +1,88 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
+import TripCard from "../../components/tripCard/TripCard";
+import TripsFilter from "../../components/tripsFilter/TripsFilter";
+import { tripsData } from "../../helpers/mockData";
+import styles from "./mainPage.module.css";
 
 const MainPage = () => {
+    const [titleData, setTitleData] = useState(null);
+    const [durationData, setDurationData] = useState(null);
+    const [levelData, setLevelData] = useState(null);
+    const [isFiltering, setIsFiltering] = useState(false);
+
+    //get data from filters
+    const handleSearchTitle = (data) => {
+        setTitleData(data);
+        console.log(data);
+    };
+
+    const handleSearchDuration = (data) => {
+        setDurationData(data);
+        console.log(data);
+    };
+
+    const handleSearchLevel = (data) => {
+        setLevelData(data);
+        console.log(data);
+    };
+
+    /* logic of filering. When one of filters starts - filter data by all 3 filters 
+    problem: коли використовую функцію як аргумент для методу filter, то коли очищую поле вводу всі картки зникають
+    Це тому що не запускається фультрація з пустою строкою
+    */
+    const trips = tripsData;
+    const filterByTitle = (trip) => {
+        if (titleData) {
+            return trip.title.toLowerCase().includes(titleData);
+        } else {
+            trip;
+        }
+    };
+
+    const filterByLevel = (trip) => {
+        if (levelData) {
+            return trip.level === levelData;
+        } else {
+            trip;
+        }
+    };
+
+    useEffect(() => {
+        if (titleData || durationData || levelData) {
+            console.log("effect works");
+            setIsFiltering(true);
+        }
+    }, [titleData, durationData, levelData]);
+
     return (
         <Layout>
-            MainPage
-            <br />
-            <Link to="/trip/1">To Trip 1</Link>
-            <br />
-            <Link to="/trip/2">To Trip 2</Link>
-            <br />
-            <Link to="/trip/3">To Trip 3</Link>
+            <main>
+                <h1 className="visually-hidden">Travel App</h1>
+                <TripsFilter
+                    onSearch={handleSearchTitle}
+                    onSelectLevel={handleSearchLevel}
+                    onSelectDuration={handleSearchDuration}
+                />
+                <section className={styles.trips}>
+                    <h2 className="visually-hidden">Trips List</h2>
+                    {/* {trips.map((trip) => (
+                        <TripCard key={trip.id} tripData={trip} />
+                    ))} */}
+                    <ul className={styles.trip_list}>
+                        {isFiltering
+                            ? trips
+                                  .filter(filterByTitle)
+                                  .filter(filterByLevel)
+                                  .map((trip) => (
+                                      <TripCard key={trip.id} tripData={trip} />
+                                  ))
+                            : trips.map((trip) => (
+                                  <TripCard key={trip.id} tripData={trip} />
+                              ))}
+                    </ul>
+                </section>
+            </main>
         </Layout>
     );
 };
